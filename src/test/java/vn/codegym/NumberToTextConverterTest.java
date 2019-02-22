1bd4d3a720fd23885a6038d3d0376688af325907
minhln101 <lengocminh@unimarmores.com>
2019-02-22 19:02:33 +0700

    remove redundant import

--- src/test/java/vn/codegym/NumberToTextConverterTest.java
+++ src/test/java/vn/codegym/NumberToTextConverterTest.java
@@ -2,7 +2,7 @@ package vn.codegym;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class NumberToTextConverterTest {
     private static NumberToTextConverter converter = new NumberToTextConverter();
